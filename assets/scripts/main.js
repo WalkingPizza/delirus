9052df0595beea07cdcef31b24b3a13c5c823087
WalkingPizza <kevin.iatauro@gmail.com>
2022-10-28 01:28:48 +0800

    Init

---
+++ assets/scripts/main.js
@@ -0,0 +1,28 @@
+const observeHeader = (e) => {
+  const header = document.querySelector("header");
+
+  if (!header) return;
+
+  if (window.scrollY > header.clientHeight)
+    header.classList.add("header--sticky");
+  else header.classList.remove("header--sticky");
+};
+
+const toggleMenu = (e) => {
+  const menu = document.querySelector(".header__menu");
+
+  if (
+    !menu ||
+    !(
+      e.target.closest(".header__hamburger") ||
+      e.target.closest(".header__menu a")
+    )
+  )
+    return;
+
+  menu.classList.toggle("header__menu--open");
+};
+
+document.addEventListener("scroll", observeHeader);
+document.addEventListener("DOMContentLoaded", observeHeader);
+document.addEventListener("click", toggleMenu);
